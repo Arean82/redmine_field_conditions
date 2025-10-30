04a5447dc61161054b2d8f84cc47312d24c6d42f
arean82 <38061300+Arean82@users.noreply.github.com>
2025-10-30 11:00:39 +0530

    Refactor ConditionsViewHook to CustomFieldViewHook

--- lib/redmine_field_conditions/hooks/custom_field_view_hook.rb
+++ lib/redmine_field_conditions/hooks/custom_field_view_hook.rb
@@ -1,6 +1,9 @@
 module RedmineFieldConditions
   module Hooks
-    class ConditionsViewHook < Redmine::Hook::ViewListener
+    #class ConditionsViewHook < Redmine::Hook::ViewListener
+    #  render_on :view_custom_fields_form_upper_box, partial: 'custom_fields/hooks/fields_conditions'
+    #end
+    class CustomFieldViewHook < Redmine::Hook::ViewListener
       render_on :view_custom_fields_form_upper_box, partial: 'custom_fields/hooks/fields_conditions'
     end
   end
